cf81855d2df2a9b3988b915d13689fabc2f793c0
NihalBihade <98096313+NihalBihade@users.noreply.github.com>
2022-03-28 17:20:45 +0530

    Add files via upload

---
+++ Student_Template_C82.js
@@ -0,0 +1,30 @@
+console.log("My first console project");
+
+            var first_name = "Ranbir";
+            var last_name = "Kapoor";
+
+
+
+            var full_name ="Ranbir Kapoor"  ;
+            console.log(full_name);
+
+            var a = 10;
+            var b = 5;
+
+Type: console.log(add)
+
+            var add = a + b   ;
+            console.log(add );
+
+
+Type: console.log(sub)
+          var sub = a - b  ;
+            console.log( sub);
+
+
+
+            var multiple = a * b  ;
+            console.log(multiple);
+
+            var divide = a / b ;
+            console.log(divide);
